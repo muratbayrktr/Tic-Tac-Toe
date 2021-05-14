ef61616488ec13f956d1f48515a0e134e696e1dc
Murat Bayraktar <muratbayrktr@users.noreply.github.com>
2021-05-14 15:51:01 +0300

    game logic implemented, readme updated

--- README.md
+++ README.md
@@ -1,10 +1,116 @@
 # tic-tac-toe
 A simple tic-tac-toe game  
 
-usage:
+usage in unix systems:
 
 ```
-$ make all
+$ make all #clears older version, compiles and runs the program.
 ```
 
-clears older version, compiles and runs the program.
+If you want to run this on windows, you can simply change the compiler from the makefile.  
+Also please change this place in the code too if you use in windows:  
+
+```
+8 #define clear "clear" /* you can change this to "cls" for windows */
+```
+
+some examples:  
+
+```
+
+                                |                        |
+                                |                        |
+              ZZ      ZZ        |          ZZZZ          |
+                ZZ   ZZ         |        ZZ    ZZ        |
+                 ZZ ZZ          |       ZZ      ZZ       |
+                  ZZZ           |       ZZ      ZZ       |
+                 ZZ ZZ          |       ZZ      ZZ       |
+                ZZ   ZZ         |        ZZ    ZZ        |
+              ZZ      ZZ        |          ZZZZ          |
+                                |                        |
+        ------------------------|------------------------|--------------------------
+        ------------------------|------------------------|--------------------------
+                                |                        |
+                                |                        |
+                                |          ZZZZ          |        ZZ      ZZ
+                                |        ZZ    ZZ        |          ZZ   ZZ
+                                |       ZZ      ZZ       |           ZZ ZZ
+                                |       ZZ      ZZ       |            ZZZ
+                                |       ZZ      ZZ       |           ZZ ZZ
+                                |        ZZ    ZZ        |          ZZ   ZZ
+                                |          ZZZZ          |        ZZ      ZZ
+                                |                        |
+        ------------------------|------------------------|--------------------------
+        ------------------------|------------------------|--------------------------
+                                |                        |
+                                |                        |
+                                |                        |        ZZ      ZZ
+                                |                        |          ZZ   ZZ
+                                |                        |           ZZ ZZ
+                                |                        |            ZZZ
+                                |                        |           ZZ ZZ
+                                |                        |          ZZ   ZZ
+                                |                        |        ZZ      ZZ
+                                |                        |
+
+
+
+
+Please input position from 1-9:
+
+
+
+```
+
+```
+                                |                        |
+                                |                        |
+              ZZ      ZZ        |          ZZZZ          |        ZZ      ZZ
+                ZZ   ZZ         |        ZZ    ZZ        |          ZZ   ZZ
+                 ZZ ZZ          |       ZZ      ZZ       |           ZZ ZZ
+                  ZZZ           |       ZZ      ZZ       |            ZZZ
+                 ZZ ZZ          |       ZZ      ZZ       |           ZZ ZZ
+                ZZ   ZZ         |        ZZ    ZZ        |          ZZ   ZZ
+              ZZ      ZZ        |          ZZZZ          |        ZZ      ZZ
+                                |                        |
+        ------------------------|------------------------|--------------------------
+        ------------------------|------------------------|--------------------------
+                                |                        |
+                                |                        |
+                 ZZZZ           |       ZZ      ZZ       |        ZZ      ZZ
+               ZZ    ZZ         |         ZZ   ZZ        |          ZZ   ZZ
+              ZZ      ZZ        |          ZZ ZZ         |           ZZ ZZ
+              ZZ      ZZ        |           ZZZ          |            ZZZ
+              ZZ      ZZ        |          ZZ ZZ         |           ZZ ZZ
+               ZZ    ZZ         |         ZZ   ZZ        |          ZZ   ZZ
+                 ZZZZ           |       ZZ      ZZ       |        ZZ      ZZ
+                                |                        |
+        ------------------------|------------------------|--------------------------
+        ------------------------|------------------------|--------------------------
+                                |                        |
+                                |                        |
+                 ZZZZ           |       ZZ      ZZ       |           ZZZZ
+               ZZ    ZZ         |         ZZ   ZZ        |         ZZ    ZZ
+              ZZ      ZZ        |          ZZ ZZ         |        ZZ      ZZ
+              ZZ      ZZ        |           ZZZ          |        ZZ      ZZ
+              ZZ      ZZ        |          ZZ ZZ         |        ZZ      ZZ
+               ZZ    ZZ         |         ZZ   ZZ        |         ZZ    ZZ
+                 ZZZZ           |       ZZ      ZZ       |           ZZZZ
+                                |                        |
+
+
+
+
+ ________      ________      ________      ___       __           ___
+|\   ___ \    |\   __  \    |\   __  \    |\  \     |\  \        |\  \
+\ \  \_|\ \   \ \  \|\  \   \ \  \|\  \   \ \  \    \ \  \       \ \  \
+ \ \  \ \\ \   \ \   _  _\   \ \   __  \   \ \  \  __\ \  \       \ \  \
+  \ \  \_\\ \   \ \  \\  \|   \ \  \ \  \   \ \  \|\__\_\  \       \ \__\
+   \ \_______\   \ \__\\ _\    \ \__\ \__\   \ \____________\       \|__|
+    \|_______|    \|__|\|__|    \|__|\|__|    \|____________|           ___
+                                                                       |\__\
+                                                                       \|__|
+
+
+Wanna play again? [y|n]:
+```
--- main.c
+++ main.c
@@ -4,7 +4,8 @@
 #define maxwidth 86
 #define tokenheight 7
 #define tokenwidth 12
-
+#define playgroundlen 9
+#define clear "clear" /* you can change this to "cls" for windows */
 
 void display_banner(void)
 {
@@ -96,35 +97,140 @@ void display_token(char token_arr[tokenheight][tokenwidth])
         printf("\n");
     } 
     printf("\n\n");
+    return ;
 }
 
-void play(char playground[maxheight][maxwidth], char X[tokenheight][tokenwidth], char O[tokenheight][tokenwidth], int filled_area[])
+int win_status(int area[])
 {
-    int position, count = 0;
+    int i, j, board[3][3] = {0}, k = 0, sum;
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            board[i][j] = area[k++];
+        }
+    }
+    /* horizontal */
+    for (i = 0; i < 3; i++) {
+        sum = 0;
+        for (j = 0; j < 3; j++) {
+            sum += board[i][j];
+        }
+        if (sum == 3) return 1; else if (sum == -3) return -1;
+    }
+    /* vertical */
+    for (j = 0; j < 3; j++) {
+        sum = 0;
+        for (i = 0; i < 3; i++) {
+            sum += board[i][j];
+        }
+        if (sum == 3) return 1; else if (sum == -3) return -1;
+    } 
+    sum = area[0] + area[4] + area[8];
+    if (sum == 3) return 1; else if (sum == -3) return -1;
+    sum = area[2] + area[4] + area[6];
+    if (sum == 3) return 1; else if (sum == -3) return -1;
+    return -2;
+}
+
+int calculate_area(int area[], int position, char token)
+{
+    int win_stat = 0;
+    /*
+    exit code 1: X token win status
+    exit code 0: position occupied
+    exit code -1: O token win status
+    exit code -2: no win, continue
+    */
+    if (area[position-1] == 0)
+    {
+        if(token == 'X') area[position-1] = 1; else area[position-1] = -1;
+        win_stat = win_status(area);
+        if (win_stat == 1) return 1; else if (win_stat == -1) return -1; else return -2;
+    }
+    return 0;
+}
+
+void print_x_won()
+{
+    int c;
+    FILE *file;
+    file = fopen("x_won.txt", "r");
+    fflush(stdin);
+    printf("\n");
+    if (file) {
+        while ((c = getc(file)) != EOF)
+            putchar(c);
+        fclose(file);
+    }
+    return ;
+}
+
+void print_o_won()
+{
+    int c;
+    FILE *file;
+    file = fopen("o_won.txt", "r");
+    fflush(stdin);
+    printf("\n");
+    if (file) {
+        while ((c = getc(file)) != EOF)
+            putchar(c);
+        fclose(file);
+    }
+    return ;
+}
+
+void print_draw()
+{
+    int c;
+    FILE *file;
+    file = fopen("draw.txt", "r");
+    fflush(stdin);
+    printf("\n");
+    if (file) {
+        while ((c = getc(file)) != EOF)
+            putchar(c);
+        fclose(file);
+    }
+    return ;
+}
+
+
+void play(char playground[maxheight][maxwidth], char X[tokenheight][tokenwidth], char O[tokenheight][tokenwidth], int area[])
+{
+    int position, count = 0, game_status;
     char token;
     
-    while (count++ < 9)
+    while (1)
     {
         display_playground(playground);
         printf(" \nPlease input position from 1-9: ");
         scanf(" %d", &position);
+        if (position < 1 || position > 9) goto jp;
         printf(" \nPlease input token [ X | O ]: ");
         scanf(" %c", &token);
 
         if (token == 'x' || token == 'o' ) token -= 32;
-        if (position < 1 || position > 9 || (token != 'X' && token != 'O')) 
+    jp: if (position < 1 || position > 9 || (token != 'X' && token != 'O')) 
         { 
             char answer;
-            printf("Would you like to quit?[Y|N]");
+            printf("\nWould you like to quit?[Y|N]");
             scanf(" %c",&answer);
             if (answer == 'y') answer -= 32;
             if (answer == 'Y') {
                 exit(0);
             } else {
+                system(clear);
                 continue;
             }
         }
-        system("clear");
+        game_status = calculate_area(area, position, token);
+        if (game_status == 0) {
+            system(clear);
+            printf("\nThis position is occupied please enter different position\n");
+            continue;
+        }
+
+        system(clear);
         switch (position)
             {
             case 1:
@@ -288,18 +394,29 @@ void play(char playground[maxheight][maxwidth], char X[tokenheight][tokenwidth],
                     
                     break;
                 }
-
             default:
-                printf("default\n");
+                printf("\nSomething went wrong :/\n");
                 break;
-            } 
-        if (count == 9)
-        {
-            scanf(" %c",token);
-        }
-        
+            }
+        count++;
+        if (game_status == 1) {
+            system(clear);
+            display_playground(playground);
+            print_x_won();
+            return ;
+        } else if (game_status == -1){
+            system(clear);
+            display_playground(playground);
+            print_o_won();
+            return ;
+        } else if (count == 9) {
+            system(clear);
+            display_playground(playground);
+            print_draw();
+            return ;
+        } 
     }
-
+    return ;
 }
 
 int main(void)
@@ -307,23 +424,37 @@ int main(void)
     char playground[maxheight][maxwidth] = {0};
     char X[tokenheight][tokenwidth] = {0}, O[tokenheight][tokenwidth] = {0};
     char x_file[6] = "X.txt", o_file[6] = "O.txt";
-    int filled_area[9] = {0};
+    
 
     display_banner();
+    printf("\n This tic-tac-toe game is little different than the usual ones. There is no order for the x and o choices. O player\n may choose to play 2 times but then x player will play 2 adjacent rounds.\n Have fun !");
 
-    printf("\nPress any key to start...\n");
 
-    if (getchar()) {
-        system("clear");
-    }
-
-    init_playground(playground);
+    
 
     
     init_token(X, x_file);
     init_token(O, o_file);
 
-    play(playground, X, O, filled_area); 
+    while (1)
+    {
+        int area[9] = {0};
+        char ans;
+        printf("\nPress any key to start...\n");
+
+        if (getchar()) {
+            system(clear);
+        }
+        init_playground(playground);
+        play(playground, X, O, area);
+        printf("\n\nWanna play again? [y|n]:\n");
+        scanf(" %c", &ans);
+        if (ans == 'Y' || ans == 'N' ) ans += 32;
+        if (ans == 'y') {
+            continue;
+        } else break;
+    }
+     
 
     return 0;
 }
